5834a667fd69e05eb957150fe0a0c560099edca9
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-01-28 21:44:42 +0100

    Modify app.py

--- serverless/hpa/app.py
+++ serverless/hpa/app.py
@@ -2,16 +2,8 @@
 from flask import Flask, request, jsonify, make_response
 import requests
 import time
-import multiprocessing
-#from multiprocessing import Pool
-#from multiprocessing import cpu_count
-
-import sys
-sys.path.insert(0, 'utils')
-
-from Monitor import MonitorThread
-from Controller import ControllerThread
-from closedLoopActuator import closedLoopActuator
+from multiprocessing import Pool
+from multiprocessing import cpu_count
 
 
 application = Flask(__name__)
@@ -20,10 +12,10 @@ application = Flask(__name__)
 def healthz():
     return make_response(jsonify({"health": "ok"}), 200)
 
-@application.route('/memory')
-def memory():
-    size = 200
-    seconds = 300
+@application.route('/memory/<int:size>/<int:time>')
+def memory(size, time):
+    size = size
+    seconds = time*60
 
     try:
       dummy = ' ' * 1024 * 1024 * size
@@ -35,31 +27,20 @@ def memory():
     return make_response("", 200)
 
 def f(x):
-    loop_count = 1000 * 1024 * 250
-    while loop_count > 0:
-      x*x
-      loop_count = loop_count - 1
+    timeout = time.time() + 60*float(set_time)  # X minutes from now
+    while True:
+        if time.time() > timeout:
+            break
+        x*x
 
-@application.route('/cpu/<int:duration>/<float:cpuLoad>')
-def cpu(duration, cpuLoad):
+@application.route('/cpu/<int:time>')
+def cpu(time):
 
-    cpu_core = 0
-    monitor = MonitorThread(cpu_core, 0.1)
-    monitor.start()
-
-    control = ControllerThread(0.1)
-    control.start()
-    control.setCpuTarget(cpuLoad)
-
-    actuator = closedLoopActuator(control, monitor, duration, cpu_core, cpuLoad/100, 0)
-    actuator.run()
-    actuator.close()
-
-    monitor.running = 0;
-    control.running = 0;
-    monitor.join()
-    control.join()
-
+    global set_time = time
+    processes = cpu_count()
+    pool = Pool(processes)
+    pool.map(f, range(processes))
+    pool.close()
     return make_response("", 200)
 
 if __name__ == '__main__':
