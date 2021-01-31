1875a1739a9ca93f9cc62744bfb16b0422e8965a
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-01-31 13:37:49 +0100

    Formate index.php

--- hpa/stress-php/index.php
+++ hpa/stress-php/index.php
@@ -1,7 +1,7 @@
 <?php
 print "<html>";
 print "<h1>CPU Stress Application for testing HPA</h1>";
-print "<a href=http://{$_SERVER['HTTP_HOST']}/cpu.php?minutes=1>CPU</a>";
-print "<a href=http://{$_SERVER['HTTP_HOST']}/memory.php?minutes=1&size=200>Memory</a> ";
+print "Stress CPU for a minute: <a href=http://{$_SERVER['HTTP_HOST']}/cpu.php?minutes=1>CPU Stress</a><br />";
+print "Allocate 200MB of Memory for a minute: <a href=http://{$_SERVER['HTTP_HOST']}/memory.php?minutes=1&size=200>Memory allocation</a>";
 print "</html>";
 ?>
