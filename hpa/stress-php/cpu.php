217d788159443e3b0747208e5b380975d02128fc
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-01-31 13:34:36 +0100

    Add set_time_limit

--- hpa/stress-php/cpu.php
+++ hpa/stress-php/cpu.php
@@ -1,10 +1,11 @@
 <?php
-$minutes = $_REQUEST['minutes'];
+$seconds = $_REQUEST['minutes'] * 60;
+ini_set('max_execution_time', $seconds+60);
 print "<h1>CPU Stress Application for testing HPA</h1>";
 print "Starting to stress CPU during $minutes minutes...";
 flush();
 
-for(set_time_limit($minutes);;);
+for(set_time_limit($minutes*60);;);
 $load = sys_getloadavg();
 print "Finihsed CPU Test Load in {$minutes} minutes. Current average load: {$load[0]}";
 ?>
