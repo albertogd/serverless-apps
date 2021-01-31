587ebba41e58ac77837884a9d15fdf01abbdc393
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-01-31 13:50:15 +0100

    Add get_memory_limit function

--- hpa/stress-php/index.php
+++ hpa/stress-php/index.php
@@ -1,7 +1,7 @@
 <?php
 print "<html>";
 print "<h1>CPU Stress Application for testing HPA</h1>";
-print "Stress CPU for a minute: <a href=http://{$_SERVER['HTTP_HOST']}/cpu.php?minutes=1>CPU Stress</a><br />";
-print "Allocate 200MB of Memory for a minute: <a href=http://{$_SERVER['HTTP_HOST']}/memory.php?minutes=1&size=200>Memory allocation</a>";
+print "Stress CPU for 2 minutes: <a href=http://{$_SERVER['HTTP_HOST']}/cpu.php?minutes=2>CPU Stress</a><br /><br />";
+print "Allocate 200MB for two minutes: <a href=http://{$_SERVER['HTTP_HOST']}/memory.php?minutes=2&size=200>Memory allocation</a>";
 print "</html>";
 ?>
--- hpa/stress-php/memory.php
+++ hpa/stress-php/memory.php
@@ -18,4 +18,30 @@ $myArray = array_fill(0, $size * 1000000, "z");
 
 sleep($minutes * 60);
 
+function get_memory_limit()
+{
+   $limit_string = ini_get('memory_limit');
+   $unit = strtolower(mb_substr($limit_string, -1 ));
+   $bytes = intval(mb_substr($limit_string, 0, -1), 10);
+   
+   switch ($unit)
+   {
+      case 'k':
+         $bytes *= 1024;
+         break 1;
+      
+      case 'm':
+         $bytes *= 1048576;
+         break 1;
+      
+      case 'g':
+         $bytes *= 1073741824;
+         break 1;
+      
+      default:
+         break 1;
+   }
+   
+   return $bytes;
+}
 ?>
