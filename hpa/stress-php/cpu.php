cc65ab63bd0ffeb88fd2bf96e041037190e60372
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-02-01 09:15:21 +0100

    Fix CPU load loop in cpu.php

--- hpa/stress-php/cpu.php
+++ hpa/stress-php/cpu.php
@@ -1,12 +1,23 @@
 <?php
 $minutes = (int)$_REQUEST['minutes'];
 $seconds = $minutes * 60;
+$timeout = time() + $seconds;
+
+// Increase Max Execution Time as default is 30 seconds
 ini_set('max_execution_time', $seconds+60);
+
+// We try to print output in real time
+ob_implicit_flush();
 print "<h1>CPU Stress Application for testing HPA</h1>";
 print "Starting to stress CPU during $minutes minutes...";
-flush();
 
-for(set_time_limit($seconds);;);
+// Infinite loop until timeout
+for ($i = 7; $current_time <= $timeout; $i++) {
+   $i = $i * $i;
+   $current_time = time();
+}
+//for(set_time_limit($seconds);;);
+
 $load = sys_getloadavg();
 print "Finihsed CPU Test Load in {$minutes} minutes. Current average load: {$load[0]}";
 ?>
