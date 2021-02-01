89d41c3ecc424a074b831a28aa69f593b82454c5
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-02-01 12:09:52 +0100

    Add CPU info in cpu.php

--- hpa/stress-php/cpu.php
+++ hpa/stress-php/cpu.php
@@ -2,18 +2,28 @@
 $minutes = (int)$_REQUEST['minutes'];
 $seconds = $minutes * 60;
 $timeout = time() + $seconds;
+$exec_cores = trim(shell_exec("grep -P '^processor' /proc/cpuinfo|wc -l"));
+
+// Get CPU Load before test
+$before_load = sys_getloadavg();
+$before_cpu = round($before_load[0]/($exec_cores + 1)*100, 0) . '%';
 
 // We try to print output in real time
 print "<h1>CPU Stress Application for testing HPA</h1>";
-print "Starting to stress CPU during $minutes minutes...";
+print "CPU before stressing CPU: {$before_cpu }<br /><br />";
+print "Starting to stress CPU during {$minutes} minutes...<br /><br />";
 
 // Infinite loop until timeout
 for ($i = 7777777; $current_time <= $timeout; $i++) {
    $i = $i * $i;
    $current_time = time();
 }
-//for(set_time_limit($seconds);;);
 
-$load = sys_getloadavg();
-print "Finihsed CPU Test Load in {$minutes} minutes. Current average load: {$load[0]}";
+// Get CPU Load after test
+$after_load = sys_getloadavg();
+$after_cpu = round($after_load[0]/($exec_cores + 1)*100, 0) . '%';
+
+print "CPU before stressing CPU: {$after_cpu }<br /><br />";
+print "Finihsed CPU Test Load in {$minutes} minutes.";
+
 ?>
