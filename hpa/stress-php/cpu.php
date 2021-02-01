d4d98560e390f7ecfd5abec040ecf8484ec8c0d2
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-02-01 09:47:03 +0100

    Add set_time_limit to cpu.php

--- hpa/stress-php/cpu.php
+++ hpa/stress-php/cpu.php
@@ -5,6 +5,7 @@ $timeout = time() + $seconds;
 
 // Increase Max Execution Time as default is 30 seconds
 ini_set('max_execution_time', $seconds+60);
+set_time_limit=$seconds+60;
 
 // We try to print output in real time
 ob_implicit_flush();
@@ -12,7 +13,7 @@ print "<h1>CPU Stress Application for testing HPA</h1>";
 print "Starting to stress CPU during $minutes minutes...";
 
 // Infinite loop until timeout
-for ($i = 7; $current_time <= $timeout; $i++) {
+for ($i = 7777777; $current_time <= $timeout; $i++) {
    $i = $i * $i;
    $current_time = time();
 }
