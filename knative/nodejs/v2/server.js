0b146ad043f38a448275763be7dfd81fc2621ace
Alberto Gonzalez de Dios <algonzal@redhat.com>
2021-02-04 21:28:07 +0100

    Fix nodejs errors

--- knative/nodejs/v2/server.js
+++ knative/nodejs/v2/server.js
@@ -1,17 +1,18 @@
-import os from 'os';
+var express = require('express');
 
-var express = require('express'),
-    app = express(),
+var app = express(),
     port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     version = '2.0';
 
 app.get('/', function(req, res) {
-    es.send('<html>');
-    res.send('<h1>NodeJS test application</h1>');
-    res.send('Version: ' + version + '<br /><br />');
-    res.send('Container: ' + os.hostname());
-    res.send('</html>');
+    message = "<html>";
+    message += "<h1>NodeJS test application</h1>";
+    message += "Version: " + version + "<br /><br />";
+    message += "Container: " + os.hostname();
+    message += "</html>";
+
+    res.send(message);
 });
 
 
--- knative/nodejs/v3/server.js
+++ knative/nodejs/v3/server.js
@@ -1,17 +1,19 @@
-import os from 'os';
-
 var express = require('express'),
-    app = express(),
+    os = require('os');
+
+var app = express(),
     port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     version = '3.0';
 
 app.get('/', function(req, res) {
-    res.send('<html>');
-    res.send('<h1>NodeJS test application</h1>');
-    res.send('Version: ' + version + '<br /><br />');
-    res.send('Container: ' + os.hostname());
-    res.send('</html>');
+    message = "<html>";
+    message += "<h1>NodeJS test application</h1>";
+    message += "Version: " + version + "<br /><br />";
+    message += "Container: " + os.hostname();
+    message += "</html>";
+
+    res.send(message);
 });
 
 
